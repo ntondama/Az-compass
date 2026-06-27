781e77ab321df799cf80b68c34f0b01f805904ee
ntntdvs32 <ntntdvs32@outlook.com>
2026-06-27 21:00:35 +0530

    feat: create streamlit ui structure1

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -1,14 +1,17 @@
+import sys
+from pathlib import Path
+
+project_root = Path(__file__).resolve().parent.parent
+sys.path.append(str(project_root))
+
 from components.header import render_header
 from components.sidebar import render_sidebar
 from pages.product_master import show_product_master
 
 
 def main():
-
     render_header()
-
     render_sidebar()
-
     show_product_master()
 
 
