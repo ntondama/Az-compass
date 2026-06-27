d4092922f38c3c28868cbc37059ec0a72fc6e245
ntntdvs32 <ntntdvs32@outlook.com>
2026-06-27 21:12:56 +0530

    correction in oruduct master  file

--- streamlit_app/pages/product_master.py
+++ streamlit_app/pages/product_master.py
@@ -6,3 +6,17 @@ sys.path.append(str(project_root))
 
 import streamlit as st
 from services.csv_service import CSVService
+
+
+def show_product_master():
+    st.subheader("📦 Product Master")
+
+    csv_service = CSVService()
+
+    products = csv_service.load_products()
+
+    st.dataframe(
+        products,
+        use_container_width=True,
+        hide_index=True
+    )
